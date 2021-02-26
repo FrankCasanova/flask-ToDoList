7bc1f7b28ec0166571bd6064f934bb5cddb884c4
Frank Casanova <frankcasanova.info@gmail.com>
2021-02-26 10:56:31 +0100

    pillando la ip del usuario y guardandolas en cookies

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, make_response,redirect
 
 app = Flask(__name__)#-----------------------esto es necesario 
 
-@app.route('/')#-----------------------------esta es la pagina en la que inicia
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))#entregamos una respuesta, que redirige a la página hello
+    response.set_cookie('user_ip', user_ip)#la coockie guarda la IP del usuario.
+
+    return response #entregamos la respuesta
+
+
+@app.route('/hello')#-----------------------------esta es la pagina en la que inicia
 def hello():
+    user_ip = request.cookies.get('user_ip') #usa la cookie con la IP del usuario mara mostrarla
     user_ip = request.remote_addr#-----------con esto obtenemos la ip del usuario
     return f'La aplicación ahora debe funcionar, su ip es {user_ip}'
 
